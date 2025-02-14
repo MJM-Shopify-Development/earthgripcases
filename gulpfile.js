d6be9852b4dfe70328fbd501ebb05620942b4bf5
Michael Joseph Miller <michael.joseph.miller@gmail.com>
2025-02-14 16:48:55 -0500

    added CI

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,32 @@
+const gulp = require('gulp');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+
 function defaultTask(cb) {
     // place code for your default task here
     cb();
-  }
-  
-  exports.default = defaultTask
+  }  
+exports.default = defaultTask  
+
+gulp.task('styles', function () {
+  return gulp
+    .src('./assets/*.css')
+    .pipe(postcss([
+      autoprefixer(),
+      cssnano(),
+    ]))
+    .pipe(gulp.dest('./assets')); 
+});
+
+gulp.task('scripts', function () {
+  return gulp
+  .src('./assets/*.js')
+    .pipe(babel({
+      presets: ['@babel/env']
+    }))
+    .pipe(uglify())
+    .pipe(gulp.dest('./assets'));
+});
